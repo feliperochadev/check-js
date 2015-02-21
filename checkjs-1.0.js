0b59feb7fe4d6442f8f71a16b9eb448d2b5730ef
felipe <feliperocha.ti@gmail.com>
2015-02-21 18:53:19 -0200

    add checkAssertLog function to show the result of assert in console

--- checkjs-1.0.js
+++ checkjs-1.0.js
@@ -1,3 +1,4 @@
+//Returns the result of assert in html, green for true and red for false
 function checkAssert(value, desc)
 {
     var ul = document.createElement("ul");
@@ -7,4 +8,25 @@ function checkAssert(value, desc)
     li.style.color = value ? "green" : "red";
     li.appendChild(document.createTextNode(desc));
     document.getElementById("results").appendChild(li);
+}
+//Returns the result of assert in console (developer tools, firebug, etc), green for true and red for false
+function checkAssertLog(value, desc)
+{
+    //if Browser is IE use console.info for pass and console.error for fail
+    //because IE does not support style in log
+    if(msie())
+    {
+        return  value ? console.info(desc) : console.error(desc);
+    }
+    value ? console.log('%c'+desc, 'color:green;') : console.log('%c'+desc, 'color:red;');
+}
+//check if browser is IE
+function msie() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE");
+    var trident = ua.indexOf("Trident");
+    if (msie > 0 || trident > 0) {
+        return true;
+    }
+    return false;
 }
