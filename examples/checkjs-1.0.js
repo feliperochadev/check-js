c493da51b93a38160d48a1344a7270683b3947bf
felipe <feliperocha.ti@gmail.com>
2015-02-24 01:27:02 -0300

    add checkTest function and examples

--- examples/checkjs-1.0.js
+++ examples/checkjs-1.0.js
@@ -1,13 +1,21 @@
+//create the container with the ul results
+window.addEventListener("load",function ()
+{
+    var container = document.createElement("div");
+    container.id = "check-results-container";
+    var ul = document.createElement("ul");
+    ul.id = "check-results";
+    container.appendChild(ul);
+    document.body.appendChild(container);
+}, false);
 //Returns the result of assert in html, green for true and red for false
 function checkAssert(value, desc)
 {
-    var ul = document.createElement("ul");
-    ul.id = "results";
-    document.body.appendChild(ul);
+    var container = document.getElementById("check-results");
     var li = document.createElement("li");
     li.style.color = value ? "green" : "red";
     li.appendChild(document.createTextNode(desc));
-    document.getElementById("results").appendChild(li);
+    container.appendChild(li);
 }
 //Returns the result of assert in console (developer tools, firebug, etc), green for true and red for false
 function checkAssertLog(value, desc)
@@ -16,7 +24,7 @@ function checkAssertLog(value, desc)
     //because IE does not support style in log
     if(msie())
     {
-      return  value ? console.info(desc) : console.error(desc);
+        return  value ? console.info(desc) : console.error(desc);
     }
     value ? console.log('%c'+desc, 'color:green;') : console.log('%c'+desc, 'color:red;');
 }
@@ -30,3 +38,21 @@ function msie() {
     }
     return false;
 }
+(function() {
+    var results;
+    this.assert = function assert(value, desc) {
+        var li = document.createElement("li");
+        li.style.color = value ? "green" : "red";
+        li.appendChild(document.createTextNode(desc));
+        results.appendChild(li);
+        if (!value) {
+            li.parentNode.parentNode.style.color = "red";
+        }
+        return li;
+    };
+    this.checkTest = function test(name, fn) {
+        results = document.getElementById("check-results");
+        results = assert(true, name).appendChild(document.createElement("ul"));
+        fn();
+    }
+})();
